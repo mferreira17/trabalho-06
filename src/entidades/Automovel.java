6d013ec1b3f8be1371e328c1cf03caf9623b31e3
marcio <marcio@thanos>
2019-04-01 14:46:29 -0300

    ajustes

--- src/entidades/Automovel.java
+++ src/entidades/Automovel.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -40,6 +41,10 @@ public class Automovel {
 	@ManyToOne
 	@JoinColumn(name = "id_modelo", referencedColumnName = "id")
 	private Modelo modelo;
+	
+	@Lob
+	@Column
+	private byte[] foto;
 	
 	
 
